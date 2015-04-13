e5e3f60c420cb6a21b991a86c2a7574168f2ce41
Guidouil <guidouil@gmail.com>
2015-04-13 17:52:13 +0200

    Adding source-sans-pro fonts

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "guidouil:reveal-js",
   summary: "A framework for easily creating beautiful presentations using HTML.",
-  version: "3.0.0_1",
+  version: "3.0.0_2",
   git: "https://github.com/guidouil/reveal.js.git"
 });
 
@@ -22,9 +22,23 @@ Package.onUse(function(api) {
 	'css/theme/solarized.css',
 	'css/theme/white.css',
 	'lib/css/zenburn.css',
-	'lib/font/league-gothic/league-gothic-webfont.eot',
-	'lib/font/league-gothic/league-gothic-webfont.ttf',
-	'lib/font/league-gothic/league-gothic-webfont.woff'
+	'lib/font/league-gothic/league-gothic.css',
+	'lib/font/league-gothic/league-gothic.eot',
+	'lib/font/league-gothic/league-gothic.ttf',
+	'lib/font/league-gothic/league-gothic.woff',
+  'lib/font/source-sans-pro/source-sans-pro-italic.eot',
+  'lib/font/source-sans-pro/source-sans-pro-italic.ttf',
+  'lib/font/source-sans-pro/source-sans-pro-italic.woff',
+  'lib/font/source-sans-pro/source-sans-pro-regular.eot',
+  'lib/font/source-sans-pro/source-sans-pro-regular.ttf',
+  'lib/font/source-sans-pro/source-sans-pro-regular.woff',
+  'lib/font/source-sans-pro/source-sans-pro-semibold.eot',
+  'lib/font/source-sans-pro/source-sans-pro-semibold.ttf',
+  'lib/font/source-sans-pro/source-sans-pro-semibold.woff',
+  'lib/font/source-sans-pro/source-sans-pro-semibolditalic.eot',
+  'lib/font/source-sans-pro/source-sans-pro-semibolditalic.ttf',
+  'lib/font/source-sans-pro/source-sans-pro-semibolditalic.woff',
+  'lib/font/source-sans-pro/source-sans-pro.css'
   ], 'client', { isAsset: true });
 
   api.addFiles([
